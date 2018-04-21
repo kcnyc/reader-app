a2932654c0b71eef362484f7db8649e442168b48
Ben Cooley <BMCooley@gmail.com>
2018-04-21 16:22:52 -0400

    add style library

---
+++ src/lib/styles/color.js
@@ -0,0 +1,10 @@
+export default {
+  red: '#cd212e',
+  redSoft: '#b33038',
+  teal: '#417f87',
+  gold: '#ab8763',
+  white: '#fafafa',
+  grey: '#cfcac6',
+  slate: '#4f4b48',
+  black: '#3b3937',
+};
